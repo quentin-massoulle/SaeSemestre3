db1adf03edea9a8e6f74c560ed0e978e36ddea6e
Rayan <123908530+rayan60180@users.noreply.github.com>
2023-12-05 09:46:29 +0100

    index.php
    Pas d'accueil

---
+++ index.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width">
+  <title>replit</title>
+  <link href="style.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+
+  <script src="script.js"></script>
+  <div class="topnav">
+    <div class="logo">
+      <img src="logo lorem ipsum.avif" alt="Logo">
+    </div>
+    <div>
+      <a class="active" href="#home">Connexion</a>
+      <a href="#news">Inscription</a>
+    </div>
+  </div>
+
+  <div class="container">
+    <h1>Association Cercle des Informaticiens Dispersés</h1>
+    <div class="contenue">
+
+      <div class="content">
+        <p>Le secrétaire de l’association,</p>
+        <p>Le mot du président,</p>
+        <p>Les dernières informations concernant l’activité de
+          l’association.</p>
+        <p>Présentation de l’association :</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          Ut enim ad
+          minim veniam.</p>
+      </div>
+      <div class="image">
+        <img src="logo lorem ipsum.avif" alt="Logo">
+      </div>I
+    </div>
+  </div>
+
+  <script src="https://replit.com/public/js/replit-badge-v2.js" theme="dark" position="bottom-right"></script>
+</body>
+
+<footer>
+  <div class="bottomfooter">
+    <div>
+      <a>Pages</a>
+      <ul>
+        <li>Page d’accueil</li>
+        <li>Mon profil</li>
+        <li>Annonces</li>
+        <li>Photos</li>
+      </ul>
+    </div>
+    <div>
+      <a>Pages légales</a>
+      <ul>
+        <li>Mentions légale</li>
+        <li>Politique de confidentialité</li>
+        <li>Préférence des cookies</li>
+      </ul>
+    </div>
+    <div class="réseau-sociaux">
+      <div class="logo">
+        <img src="logo lorem ipsum.avif" alt="Logo">
+      </div>
+      <div class="logo">
+        <img src="logo lorem ipsum.avif" alt="Logo">
+      </div>
+      <div class="logo">
+        <img src="logo lorem ipsum.avif" alt="Logo">
+      </div>
+      <div class="logo">
+        <img src="logo lorem ipsum.avif" alt="Logo">
+      </div>
+    </div>
+    <div class="logo">
+      <img src="logo lorem ipsum.avif" alt="Logo">
+    </div>
+  </div>
+</footer>
+
+</html>  
