02df67b949b2784e9c64ebc3d1f035a96acf96c7
quentin-massoulle <quentin.massoulle@etud.u-picardie.fr>
2023-12-07 14:56:51 +0100

    implementation d'une inscription appartire d'un fichier csv

--- scriptPHP/inscription.php
+++ scriptPHP/inscription.php
@@ -1,12 +1,16 @@
 <?php
 
 function inscriptionUtilisateur($email, $prenom, $nom, $promo) {
-    // Paramètres de connexion à la base de données
+    if (utilisateurExiste($nom,$prenom)){
+        echo "utilisateur deja cree";
+    }
+    else if (utilisateurDansCSV($nom, $prenom, "../Utilisateur/Diplômés-IUT-1987-2013.csv")){
+        // Paramètres de connexion à la base de données
     $serveur = "localhost";
     $utilisateur = "root";
     $mot_de_passe = "";
     $nomBaseDeDonnees = "BaseCID";
-    include './envoiemdp';
+    include './envoiemail';
 
     // Connexion à la base de données
     $connexion = new mysqli($serveur, $utilisateur, $mot_de_passe, $nomBaseDeDonnees);
@@ -64,6 +68,9 @@ function inscriptionUtilisateur($email, $prenom, $nom, $promo) {
 
     // Fermer la connexion
     $connexion->close();
+    }
+    
+    
 }
 
 
@@ -79,6 +86,96 @@ function genererMotDePasse($longueur = 12) {
 
     return $motDePasse;
 }
+function utilisateurExiste($nom,$prenom) {
+    // Paramètres de connexion à la base de données
+    $serveur = "localhost";
+    $utilisateur = "root";
+    $mot_de_passe = "";
+    $nomBaseDeDonnees = "BaseCID";
+
+    // Connexion à la base de données
+    $connexion = new mysqli($serveur, $utilisateur, $mot_de_passe, $nomBaseDeDonnees);
+
+    // Vérifier la connexion
+    if ($connexion->connect_error) {
+        die("Échec de la connexion à la base de données : " . $connexion->connect_error);
+    }
+
+    // Requête pour vérifier si l'utilisateur existe
+    $requete = $connexion->prepare("SELECT ID_Utilisateur FROM utilisateur WHERE Prénom = ? and nom = ?");
+    $requete->bind_param("ss",$prenom,$nom);
+    
+    // Exécution de la requête
+    $requete->execute();
+
+    // Récupération des résultats
+    $resultat = $requete->get_result();
+
+    // Fermeture de la requête
+    $requete->close();
+
+    // Fermeture de la connexion
+    $connexion->close();
+
+    // Retourner true si l'utilisateur existe, false sinon
+    return $resultat->num_rows > 0;
+}
+
+function utilisateurDansCSV($nomRecherche, $prenomRecherche, $cheminFichierCSV) {
+    // Ouvrir le fichier CSV en mode lecture
+    $fichier = fopen($cheminFichierCSV, 'r');
+
+    // Vérifier si le fichier est ouvert avec succès
+    if ($fichier) {
+        // Parcourir chaque ligne du fichier
+        while (($ligne = fgetcsv($fichier)) !== false) {
+            // Vérifier si le tableau d'informations est valide et a au moins une colonne
+            if (is_array($ligne) && count($ligne) >= 1) {
+                // Utiliser str_getcsv pour analyser la ligne
+                $infos = str_getcsv($ligne[0]);
+
+                // Vérifier si la première colonne existe et n'est pas nulle
+                if (isset($infos[0]) && $infos[0] !== null) {
+                    $infos[0] = retirerPrefixe($infos[0]);
+                    // Diviser la chaîne en un tableau de mots
+                    $mots = explode(' ', $infos[0]);
+
+                    // Extraire le nom et le prénom
+                    if (count($mots) >= 2) {
+                        $nom = trim($mots[0]); // Correction : le nom est le premier élément
+                        $prenom = trim($mots[1]);
+                       
+
+                        // Vérifier si le nom et le prénom correspondent
+                        if (stripos($nom, $nomRecherche) !== false && stripos($prenom, $prenomRecherche) !== false) {
+                            // Afficher le nom et le prénom
+                            echo "Nom : $nom, Prénom : $prenom";
+
+                            // Fermer le fichier et retourner true si l'utilisateur est trouvé
+                            fclose($fichier);
+                            return true;
+                        }
+                    }
+                }
+            }
+        }echo ' personne de ce nom ds la base ';
+    }
+
+    // Retourner false si l'utilisateur n'est pas trouvé ou s'il y a une erreur
+    fclose($fichier);
+    return false;
+}
+function retirerPrefixe($chaine) {
+    // Liste des préfixes à retirer
+    $prefixes = array('M.', 'Mlle');
+
+    // Remplacer les préfixes par une chaîne vide
+    $chaineSansPrefixe = str_replace($prefixes, '', $chaine);
+
+    // Retourner la chaîne résultante
+    return trim($chaineSansPrefixe);
+}
+
 
 // Vérifier si le formulaire a été soumis
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
@@ -90,5 +187,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Appeler la fonction d'inscription
     inscriptionUtilisateur($email, $prenom, $nom, $promo);
 }
+
 
 ?>
